4954894e9666a6494957ce88df59825a5b07d4b0
Raziye <rosemiller.developer@gmail.com>
2022-08-20 13:16:00 +0430

    Linking between pages in our SPA

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,12 +1,16 @@
-// our-domin.com/news/s.th
 import { useRouter } from 'next/router';
 
 function DetailPage() {
     const router = useRouter();
+    const newsId = router.query.newsId;  
     
-    console.log(router.query.newsId);
     
-    return <h1>The Detail Page</h1>
+    return (
+			<>
+				<h1>The Detail Page</h1>
+                <p>{`Here is all you need to know about ${newsId}`}</p>
+			</>
+		)
 }
 
 export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
-// our-domin.com/news  
+// important: by Link you can navigate around your SPA without any request for new HTML page
+// Although any path in our app that might be searched or entered directly from the begining,
+// Is supported by the server-side pre-rendering feature so visible by search engine crawlers
+import Link from "next/link"
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+	return (
+		<>
+			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/latest-react-articles">Latest React Articles</Link>
+				</li>
+				<li>
+					<Link href="/news/key-feauters-of-nextjs">
+						Key Features of NextJS
+					</Link>
+				</li>
+			</ul>
+		</>
+	)
 }
 
-export default NewsPage;
+export default NewsPage
